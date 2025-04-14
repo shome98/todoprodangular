0598ca7e0128d85a1cd78b913419050410cba3fb
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 01:15:28 +0530

    ng generate module todos --route todos --module app.module

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  standalone: false,
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.scss'
+})
+export class TodosComponent {
+
+}
