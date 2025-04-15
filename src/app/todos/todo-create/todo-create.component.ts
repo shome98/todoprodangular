46675cfc93d5e6ddf484962d76066226f27b847a
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 11:23:54 +0530

    ng generate component todo-create

---
+++ src/app/todos/todo-create/todo-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-create',
+  standalone: false,
+  templateUrl: './todo-create.component.html',
+  styleUrl: './todo-create.component.scss'
+})
+export class TodoCreateComponent {
+
+}
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -5,13 +5,15 @@ import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './todos.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
 
 
 @NgModule({
   declarations: [
     TodosComponent,
     TodoListComponent,
-    TodoDetailComponent
+    TodoDetailComponent,
+    TodoCreateComponent
   ],
   imports: [
     CommonModule,
