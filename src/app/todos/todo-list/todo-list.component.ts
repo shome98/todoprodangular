c8984aa521c33dd45e39a8c435d14269b0c9f53f
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 12:25:29 +0530

    subscribe

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo.model';
+import { TodoService } from '../todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+  todos: Todo[] = [];
+  constructor(private todoService: TodoService, private router: Router) { }
+  ngOnInit(): void {
+    this.loadTodos();
+  }
 
+  loadTodos(): void{
+    this.todoService.getAllTodos().subscribe(todos => this.todos = todos);
+  }
+
+  deleteTodo(id: number): void{
+    this.todoService.deleteTodo(id).subscribe(() => this.loadTodos());
+  }
+
+  viewTodo(id: number): void{
+    this.router.navigate(['/todos', id]);
+  }
+
+  editTodo(id: number): void{
+    this.router.navigate(['/todos/edit', id]);
+  }
 }
