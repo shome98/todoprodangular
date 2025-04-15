1e4d82005414cc56cc9157bc6d2307d8ac44e7eb
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 00:18:40 +0530

    todo create

--- src/app/todos/todo-create/todo-create.component.ts
+++ src/app/todos/todo-create/todo-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TodoService } from '../todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-create',
@@ -7,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './todo-create.component.scss'
 })
 export class TodoCreateComponent {
+  todoForm: FormGroup;
+  constructor(private fb: FormBuilder,private todoService:TodoService,private router:Router) {
+    this.todoForm = this.fb.group({
+      title: ['', Validators.required],
+      completed: [false],
+    });
+  }
 
+  onSubmit(): void{
+    if (this.todoForm.valid) {
+      this.todoService.createTodo(this.todoForm.value).subscribe(() => {
+        this.router.navigate(['/todos']);
+      });
+    }
+  }
+
+  goBack(): void{
+    this.router.navigate(['/todos']);
+  }
 }
