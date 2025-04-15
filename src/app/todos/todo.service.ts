5321710d4d07231fc85e75024ca3cd11306e87ea
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 12:10:00 +0530

    todo service

--- src/app/todos/todo.service.ts
+++ src/app/todos/todo.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  getAllTodos(): Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  getTodoById(id: number): Observable<Todo>{
+    return this.http.get<Todo>(`${this.apiUrl}/${id}`);
+  }
+
+  createTodo(todo: Omit<Todo, 'id'>): Observable<Todo>{
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  updateTodo(id: number, todo: Todo): Observable<Todo>{
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
+  }
+
+  deleteTodo(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
 }
