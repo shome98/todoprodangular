3ec930488b53891e900fa403fd9833677e6ad50a
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 23:39:54 +0530

    todo details

--- src/app/todos/todo-detail/todo-detail.component.ts
+++ src/app/todos/todo-detail/todo-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo.model';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from '../todo.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-todo-detail',
@@ -6,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './todo-detail.component.html',
   styleUrl: './todo-detail.component.scss'
 })
-export class TodoDetailComponent {
+export class TodoDetailComponent implements OnInit {
+  todo: Todo | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private todoService: TodoService,
+    private location: Location
+  ) { }
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.getTodo(id);
+  }
+  
+  getTodo(id: number) {
+    this.todoService.getTodoById(id).subscribe((todo) => this.todo = todo);
+  }
 
+  goBack(): void{
+    this.location.back();
+  }
 }
