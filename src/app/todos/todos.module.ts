7ba5dc619ecb64c28fc326b728b01829645e27eb
Sowparna Shome <soupornoshome@gmail.com>
2025-04-15 23:41:10 +0530

    FormsModule,     ReactiveFormsModule add

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -7,6 +7,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 import { TodoCreateComponent } from './todo-create/todo-create.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import { TodoEditComponent } from './todo-edit/todo-edit.component';
   ],
   imports: [
     CommonModule,
-    TodosRoutingModule
+    TodosRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TodosModule { }
