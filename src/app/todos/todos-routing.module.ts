3df6300ef9eab43f570e6095bdeaff693fce8ba3
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 00:35:28 +0530

    routing add

--- src/app/todos/todos-routing.module.ts
+++ src/app/todos/todos-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodosComponent } from './todos.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
-const routes: Routes = [{ path: '', component: TodosComponent }];
+const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'create', component: TodoCreateComponent },
+  { path: ':id', component: TodoDetailComponent },
+  { path: 'edit/:id', component: TodoEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
