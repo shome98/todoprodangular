2fbc7363164244d5404ad8e54707da6156dc0c5a
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:46:46 +0530

    component routing add

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [{ path: '', component: ProductListComponent },
+  { path: 'create', component: ProductCreateComponent },
+  { path: ':id', component: ProductDetailComponent },
+  { path: 'edit/:id', component: ProductEditComponent },];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
