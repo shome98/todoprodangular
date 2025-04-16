ffa4aa471660d0baad1bbcb624cfd84c4601918b
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:40:03 +0530

    product detail

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
+  product: Product | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.getProduct(id);
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProductById(id).subscribe((product) => (this.product = product));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
