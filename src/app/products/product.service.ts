803128dfdc2acc8001489aa9f9646b00e2c0508b
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 21:05:12 +0530

    observable add

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -8,11 +8,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductService {
   private apiUrl = 'https://fakestoreapi.com/products';
+  private productsSubject = new BehaviorSubject<Product[]>([]);
+  public products = this.productsSubject.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadInitialProducts(); 
+  }
+
+  private loadInitialProducts(): void {
+    this.http.get<Product[]>(this.apiUrl).subscribe((products) => {
+      this.productsSubject.next(products);
+    });
+  }
 
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.products; 
   }
 
   getProductById(id: number): Observable<Product> {
@@ -20,14 +30,31 @@ export class ProductService {
   }
 
   createProduct(product: Omit<Product, 'id'>): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, product);
+    return this.http.post<Product>(this.apiUrl, product).pipe(
+      tap((newProduct) => {
+        const currentProducts = this.productsSubject.getValue();
+        this.productsSubject.next([...currentProducts, { ...newProduct, id: Date.now() }]);
+      })
+    );
   }
 
   updateProduct(id: number, product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product).pipe(
+      tap((updatedProduct) => {
+        const currentProducts = this.productsSubject.getValue();
+        const updatedProducts = currentProducts.map((p) => (p.id === id ? { ...p, ...updatedProduct } : p));
+        this.productsSubject.next(updatedProducts);
+      })
+    );
   }
 
   deleteProduct(id: number): Observable<unknown> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        const currentProducts = this.productsSubject.getValue();
+        const updatedProducts = currentProducts.filter((product) => product.id !== id);
+        this.productsSubject.next(updatedProducts);
+      })
+    );
   }
 }
--- src/app/todos/todo.service.ts
+++ src/app/todos/todo.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Todo } from './todo.model';
 
 @Injectable({
@@ -8,28 +8,55 @@ import { Todo } from './todo.model';
 })
 export class TodoService {
 
-  constructor(private http: HttpClient) { }
+  private todosSubject = new BehaviorSubject<Todo[]>([]);
+  public todos = this.todosSubject.asObservable();
+  constructor(private http: HttpClient) {
+    this.loadInitialTodos();
+   }
 
   private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-  getAllTodos(): Observable<Todo[]>{
-    return this.http.get<Todo[]>(this.apiUrl);
+  private loadInitialTodos(): void {
+    this.http.get<Todo[]>(this.apiUrl).subscribe((todos) => {
+      this.todosSubject.next(todos);
+    });
   }
 
-  getTodoById(id: number): Observable<Todo>{
+  getAllTodos(): Observable<Todo[]> {
+    return this.todos; 
+  }
+
+  getTodoById(id: number): Observable<Todo> {
     return this.http.get<Todo>(`${this.apiUrl}/${id}`);
   }
 
-  createTodo(todo: Omit<Todo, 'id'>): Observable<Todo>{
-    return this.http.post<Todo>(this.apiUrl, todo);
+  createTodo(todo: Omit<Todo, 'id'>): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo).pipe(
+      tap((newTodo) => {
+        const currentTodos = this.todosSubject.getValue();
+        this.todosSubject.next([...currentTodos, { ...newTodo, id: Date.now() }]);
+      })
+    );
   }
 
-  updateTodo(id: number, todo: Todo): Observable<Todo>{
-    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
+  updateTodo(id: number, todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo).pipe(
+      tap((updatedTodo) => {
+        const currentTodos = this.todosSubject.getValue();
+        const updatedTodos = currentTodos.map((t) => (t.id === id ? { ...t, ...updatedTodo } : t));
+        this.todosSubject.next(updatedTodos);
+      })
+    );
   }
 
-  deleteTodo(id: number) {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+  deleteTodo(id: number): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        const currentTodos = this.todosSubject.getValue();
+        const updatedTodos = currentTodos.filter((todo) => todo.id !== id);
+        this.todosSubject.next(updatedTodos);
+      })
+    );
   }
 
 }
