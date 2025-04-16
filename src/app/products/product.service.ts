0bf41079298e7cc83745220c1e607d0f5b247a74
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:36:47 +0530

    product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = 'https://fakestoreapi.com/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: Omit<Product, 'id'>): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
