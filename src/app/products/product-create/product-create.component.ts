0bd66a11bbb657ac200df4740ef3e2be74e93948
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:43:19 +0530

    create product

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrl: './product-create.component.scss'
 })
 export class ProductCreateComponent {
+  productForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private productService: ProductService, private router: Router) {
+    this.productForm = this.fb.group({
+      title: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      description: [''],
+      image: [''],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      this.productService.createProduct(this.productForm.value).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
