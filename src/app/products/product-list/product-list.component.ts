c3a34cce2adb9305652fde5ec4f67cd24014226f
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:38:16 +0530

    product list

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAllProducts().subscribe((products) => (this.products = products));
+  }
+
+  deleteProduct(id: number): void {
+    this.productService.deleteProduct(id).subscribe(() => this.loadProducts());
+  }
+
+  viewProduct(id: number): void {
+    this.router.navigate(['/products', id]);
+  }
+
+  editProduct(id: number): void {
+    this.router.navigate(['/products/edit', id]);
+  }
 }
