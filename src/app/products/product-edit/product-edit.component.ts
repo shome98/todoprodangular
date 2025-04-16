463f9ef08d8008e5ddf0a5af1c595110f16620f7
Sowparna Shome <soupornoshome@gmail.com>
2025-04-16 20:45:41 +0530

    edit product

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,5 +11,46 @@ import { Component } from '@angular/core';
   styleUrl: './product-edit.component.scss'
 })
 export class ProductEditComponent {
+  productForm: FormGroup;
+  productId: number | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private productService: ProductService,
+    private router: Router,
+    private location: Location
+  ) {
+    this.productForm = this.fb.group({
+      title: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      description: [''],
+      image: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    this.productId = Number(this.route.snapshot.paramMap.get('id'));
+    if (this.productId) {
+      this.loadProduct(this.productId);
+    }
+  }
+
+  loadProduct(id: number): void {
+    this.productService.getProductById(id).subscribe((product) => {
+      this.productForm.patchValue(product);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid && this.productId) {
+      this.productService.updateProduct(this.productId, this.productForm.value).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
